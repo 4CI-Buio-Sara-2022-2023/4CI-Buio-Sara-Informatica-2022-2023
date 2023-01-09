9afa138b9956b94d62544914f234b9e501e8a244
graziacobello <57944379+graziacobello@users.noreply.github.com>
2023-01-09 10:40:55 +0100

    Update Televisore.java

--- 2022-2023/buio_sara_Televisione/bin/VER2/Televisore.java
+++ 2022-2023/buio_sara_Televisione/bin/VER2/Televisore.java
@@ -16,8 +16,11 @@ public class Televisore{
         this.acceso = false;
     }
     public Televisore(int pollici, String schermo, String colore, int canale, int volume, int luminos, boolean acceso){
-	this.pollici = pollici;
-	this.schermo = schermo;
+	    if(pollici>0) this.pollici = pollici;
+	    else this.pollici = 0;
+	    if (schermo!=null) this.schermo = schermo;
+	    else this.schermo = "anonimo";
+	    //correggi tutto prima che mi arrabbi
 	this.colore = colore;
 	this.canale = canale;
 	this.volume = volume;
@@ -36,26 +39,28 @@ public class Televisore{
         return this.schermo;
     }
     private void setSchermo(String s){
-        if (s == "lcd"){
+        if (s == "lcd"|| s == "oled" || s == "miniled"){
             this.schermo = s;
         }
-        if (s == "oled"){
+	
+        /*if (s == "oled"){
             this.schermo = s;
         }
         if (s == "miniled"){
             this.schermo = s;
-        }
+        }*/
     }
     public String getColore(){
         return this.colore;
     }
     private void setColore(String col){
-        if (col == "black"){
+        if (col != null)
+		if (col == "black" !! col == "grey"){
+            	this.colore = col;
+        	}
+        /*if (col == "grey"){
             this.colore = col;
-        }
-        if (col == "grey"){
-            this.colore = col;
-        }
+        }*/
     }
     public void on(){
         this.acceso = true;
@@ -67,25 +72,26 @@ public class Televisore{
         return this.canale;
     }
     public void setCanale(int can){
-        this.canale = can;
+	    if(can>0) this.canale = can;
     }
     public void aumentaCanale(){
         this.canale++;
     }
     public void diminuisciCanale(){
+	    if (this.canale>1)
         this.canale--;
     }
     public int getVolume(){
         return this.volume;
     }
     public void setVolume(int vol){
-        this.volume = vol;
+        if(vol>0) this.volume = vol;
     }
     public void aumentaVolume(){
         this.volume++;
     }
     public void diminuisciVolume(){
-        this.volume--;
+        if(this.volume>1) this.volume--;
     }
     public int getLuminos(){
         return this.luminos;
@@ -94,7 +100,7 @@ public class Televisore{
         this.luminos++;
     }
     public void diminusciLumions(){
-        this.luminos--;
+        if (this.luminos>1) this.luminos--;
     }
     public String toString(){
         String out = "I pollici sono: " + this.pollici + "\n" + "Lo schermo è: " + this.schermo + "\n" + "Il colore della televisione è: " + this.colore + "\n";
